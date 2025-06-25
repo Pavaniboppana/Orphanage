ba9363011e8311f5e406744501ff08ba99ca9643
Pavaniboppana <pavaniboppana123@gmail.com>
2025-06-25 17:00:37 +0530

    hey

--- app/src/Component/Home/Home.jsx
+++ app/src/Component/Home/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
+<p>hello</p>
 
 </>
   )
---
+++ app/src/Component/Medical/Blooddonor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blooddonor = () => {
+  return (
+    <div>Blooddonor</div>
+  )
+}
+
+export default Blooddonor
